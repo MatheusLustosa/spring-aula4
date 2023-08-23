0c98dd22167fb39159ed39904b3b27ef2accd4c4
MatheusLustosa <matheusclc@hotmail.com>
2023-08-23 20:19:14 -0300

    teste

--- src/main/java/next/school/cesar/springaula4/SpringAula4Application.java
+++ src/main/java/next/school/cesar/springaula4/SpringAula4Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAula4Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAula4Application.class, args);
 	}
